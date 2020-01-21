a19a0f03ad2b90173082e822adc58250d648917d
coderbase-it <coderbaseit@gmail.com>
2020-01-21 19:26:02 +0100

    1 file modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+
 import { HttpClientModule} from '@angular/common/http';
-import { ImageComponent } from './image/image.component';
+
 import { PonyComponent } from './pony/pony.component';
 import { RaceComponent } from './race/race.component';
 import { PonyImageComponent } from './pony-image/pony-image.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ], 
-  declarations: [ AppComponent, HelloComponent, ImageComponent, PonyComponent, RaceComponent, PonyImageComponent ],
+  declarations: [ AppComponent, PonyComponent, RaceComponent, PonyImageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
